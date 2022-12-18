2de0414062c54e74608aae6aac7b67a367a0b4b3
Justin Dela Cruz <Traxiliongames@gmail.com>
2022-12-17 23:27:26 -0500

    Balloons will be in a GameObject List

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -6,31 +6,21 @@ public class GameLogic : MonoBehaviour
 {
     float durationUntilNextBalloon;
     Sprite circleTexture;
+    List<GameObject> balloons;
 
     void Start()
     {
+        balloons = new List<GameObject>();
         NetworkedClientProcessing.SetGameLogic(this);
     }
-    void Update()
-    {
-       /* durationUntilNextBalloon -= Time.deltaTime;
 
-        if(durationUntilNextBalloon < 0)
-        {
-            durationUntilNextBalloon = 1f;
-
-            float screenPositionXPercent = Random.Range(0.0f, 1.0f);
-            float screenPositionYPercent = Random.Range(0.0f, 1.0f);
-            Vector2 screenPosition = new Vector2(screenPositionXPercent * (float)Screen.width, screenPositionYPercent * (float)Screen.height);
-            SpawnNewBalloon(screenPosition);
-        }*/
-    }
     public void SpawnNewBalloon(Vector2 screenPosition, int id)
     {
         if(circleTexture == null)
             circleTexture = Resources.Load<Sprite>("Circle");
 
         GameObject balloon = new GameObject("Balloon");
+        balloons.Add(balloon);
 
         balloon.AddComponent<SpriteRenderer>();
         balloon.GetComponent<SpriteRenderer>().sprite = circleTexture;
